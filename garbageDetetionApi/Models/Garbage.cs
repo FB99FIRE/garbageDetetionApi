65cdae8f0bee3f9529d590649095908e0b7a79cb
smickel20 <75023071+smickel20@users.noreply.github.com>
2025-05-28 11:00:56 +0200

    added model garbage

---
+++ garbageDetetionApi/Models/Garbage.cs
@@ -0,0 +1,18 @@
+﻿namespace garbageDetetionApi.Models
+{
+    public class Garbage
+    {
+        public int Id { get; set; }
+        public string Detected { get; set; }
+        public decimal Confidence_score { get; set; }
+
+        public string Weather {  get; set; }
+
+        public decimal Temp { get; set; }
+
+        public decimal humidity { get; set; }
+        public decimal Windspeed {  get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
