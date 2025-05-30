0eda031a85f6f5de91a9cd1797d9a987d7c28bd7
smickel20 <75023071+smickel20@users.noreply.github.com>
2025-05-30 14:46:41 +0200

    init migrations

---
+++ garbageDetetionApi/Migrations/20250530124127_InitCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace garbageDetetionApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Garbages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Detected = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Confidence_score = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Weather = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temp = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Humidity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Windspeed = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Garbages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Garbages");
+        }
+    }
+}
