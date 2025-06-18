13372f54548f73a729b37530a0a18e42409127a6
BlasixMac <blasixcorp@gmail.com>
2025-06-18 10:10:43 +0200

    Add seeder for ApiKeys

--- garbageDetetionApi/Configurations/Garbageconfiguration.cs
+++ garbageDetetionApi/Configurations/Garbageconfiguration.cs
@@ -4,86 +4,21 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LitterLinq.Configurations
 {
-    public class LitterConfiguration : IEntityTypeConfiguration<Garbage>
+    public class LitterConfiguration : IEntityTypeConfiguration<ApiKey>
     {
-        public void Configure(EntityTypeBuilder<Garbage> builder)
+        public void Configure(EntityTypeBuilder<ApiKey> builder)
         {
-            builder.ToTable("Garbages");
+            builder.ToTable("ApiKeys");
             builder.HasData(
-                new Garbage
-                {
-                    Id = 1,
-                    DetectedObject = "Plastic Bottle",
-                    ImageName = "plastic_bottle",
-                    ConfidenceScore = 0.95m,
-                    CameraId = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
-                    Longitude = 4.778720m,
-                    Latitude = 51.591415m,
-                    Weather = "Sunny",
-                    Temp = 22.5m,
-                    Humidity = 45.0m,
-                    WindSpeed = 5.2m,
-                    TimeStamp = new DateTime(2025, 5, 15, 10, 0, 0)
+                new ApiKey
+                { 
+                    Id = Guid.Parse("12a12a12-a12a-12aa-12aa-12a12a12a12a"),
+                    Type = "write"
                 },
-                new Garbage
-                {
-                    Id = 2,
-                    DetectedObject = "Can",
-                    ImageName = "can",
-                    ConfidenceScore = 0.89m,
-                    CameraId = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
-                    Longitude = 4.778720m,
-                    Latitude = 51.591415m,
-                    Weather = "Cloudy",
-                    Temp = 18.3m,
-                    Humidity = 55.0m,
-                    WindSpeed = 3.8m,
-                    TimeStamp = new DateTime(2025, 5, 15, 11, 0, 0)
-                },
-                new Garbage
-                {
-                    Id = 3,
-                    DetectedObject = "Paper",
-                    ImageName = "paper",
-                    ConfidenceScore = 0.92m,
-                    CameraId = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
-                    Longitude = 4.778720m,
-                    Latitude = 51.591415m,
-                    Weather = "Rainy",
-                    Temp = 16.0m,
-                    Humidity = 80.0m,
-                    WindSpeed = 7.1m,
-                    TimeStamp = new DateTime(2025, 5, 15, 12, 0, 0)
-                },
-                new Garbage
-                {
-                    Id = 4,
-                    DetectedObject = "Glass",
-                    ImageName = "glass",
-                    ConfidenceScore = 0.88m,
-                    CameraId = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
-                    Longitude = 4.778720m,
-                    Latitude = 51.591415m,
-                    Weather = "Windy",
-                    Temp = 20.0m,
-                    Humidity = 50.0m,
-                    WindSpeed = 12.0m,
-                    TimeStamp = new DateTime(2025, 5, 15, 13, 0, 0)
-                },
-                new Garbage
-                {
-                    Id = 5,
-                    DetectedObject = "Food Wrapper",
-                    ImageName = "food_wrapper",
-                    ConfidenceScore = 0.90m,
-                    CameraId = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
-                    Longitude = 4.778720m,
-                    Latitude = 51.591415m,
-                    Weather = "Sunny",
-                    Temp = 24.0m,
-                    Humidity = 40.0m,
-                    WindSpeed = 4.5m,
-                    TimeStamp = new DateTime(2025, 5, 15, 14, 0, 0)
+                new ApiKey
+                { 
+                    Id = Guid.Parse("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"),
+                    Type = "read"
                 }
             );
         }
--- garbageDetetionApi/Migrations/20250618080451_AddApiKeys.cs
+++ garbageDetetionApi/Migrations/20250618080451_AddApiKeys.cs
@@ -49,15 +49,12 @@ namespace garbageDetetionApi.Migrations
                 });
 
             migrationBuilder.InsertData(
-                table: "Garbages",
-                columns: new[] { "Id", "CameraId", "ConfidenceScore", "DetectedObject", "Humidity", "ImageName", "Latitude", "Longitude", "Temp", "TimeStamp", "Weather", "WindSpeed" },
+                table: "ApiKeys",
+                columns: new[] { "Id", "Type" },
                 values: new object[,]
                 {
-                    { 1, new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), 0.95m, "Plastic Bottle", 45.0m, "plastic_bottle", 51.591415m, 4.778720m, 22.5m, new DateTime(2025, 5, 15, 10, 0, 0, 0, DateTimeKind.Unspecified), "Sunny", 5.2m },
-                    { 2, new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), 0.89m, "Can", 55.0m, "can", 51.591415m, 4.778720m, 18.3m, new DateTime(2025, 5, 15, 11, 0, 0, 0, DateTimeKind.Unspecified), "Cloudy", 3.8m },
-                    { 3, new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), 0.92m, "Paper", 80.0m, "paper", 51.591415m, 4.778720m, 16.0m, new DateTime(2025, 5, 15, 12, 0, 0, 0, DateTimeKind.Unspecified), "Rainy", 7.1m },
-                    { 4, new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), 0.88m, "Glass", 50.0m, "glass", 51.591415m, 4.778720m, 20.0m, new DateTime(2025, 5, 15, 13, 0, 0, 0, DateTimeKind.Unspecified), "Windy", 12.0m },
-                    { 5, new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), 0.90m, "Food Wrapper", 40.0m, "food_wrapper", 51.591415m, 4.778720m, 24.0m, new DateTime(2025, 5, 15, 14, 0, 0, 0, DateTimeKind.Unspecified), "Sunny", 4.5m }
+                    { new Guid("12a12a12-a12a-12aa-12aa-12a12a12a12a"), "write" },
+                    { new Guid("d3c1f8b2-4e5f-4a2b-9c3e-8f1b2c3d4e5f"), "read" }
                 });
         }
 
